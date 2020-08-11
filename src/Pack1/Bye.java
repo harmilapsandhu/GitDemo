ac7ea2cb15197abf52919e10a5b3a1c9ff945784
Harmilap Sandhu <harmilapsandhu@gmail.com>
2020-08-11 17:50:27 -0400

    New shi

--- src/Pack1/Bye.java
+++ src/Pack1/Bye.java
@@ -4,9 +4,16 @@ import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class Bye {
+	
+	@BeforeTest
+	public void Unknown()
+	{
+		System.out.println("This is the new shit");
+	}
 	
 	@Test(groups={"Arsenal"})
 	public void Demo3()
